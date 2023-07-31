30bcf1add6a3e7bdc5db155802991e0857c248e7
Souvik Raj Singh <souvikrajsingh@gmail.com>
2023-07-31 20:50:44 +0530

    my-skills-update

--- js/app.js
+++ js/app.js
@@ -5,12 +5,13 @@ const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
 
 window.addEventListener("scroll", () => {
-    console.log("he;ll");
+    skillsCounter();
 });
 
 
+/* ----------Sticky Navbar ---------*/
 
-// sticky navbar 
+
 function stickyNavbar(){
     header.classList.toggle("scrolled" , window.pageYOffset > 0);
 }
@@ -24,6 +25,8 @@ window.addEventListener("scroll", stickyNavbar);
 // scroll bar animation using scrollReveal.js, added to the showcase-info class
 // from site https://scrollrevealjs.org/guide/hello-world.html
 
+/* ---------- Reveal Animation ---------*/
+
 let sr=  ScrollReveal({
     duration:2500,
     distance: "60px",
@@ -34,4 +37,16 @@ sr.reveal(".showcase-info",{delay:500});
 sr.reveal(".showcase-image",{origin: "top", delay:700});
 
 
+/* ---------- Reveal Animation ---------*/
 
+function hasReached(element) {
+    let topPosition = element.getBoundingClientRect().top;
+    
+    if(window.innerHeight >= topPosition + element.offsetHeight) return true;
+    return false;
+}
+
+function skillsCounter() {
+    if(!hasReached(first_skill)) return ;
+   progress_bars.forEach(p => p.computedStyleMap.animation = "progress 2s ease-in-out ");
+}
